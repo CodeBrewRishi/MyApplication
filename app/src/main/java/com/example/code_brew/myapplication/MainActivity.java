25ae51f79959c18d30a3079708e9778e19d1cc14
code-brew <rishi.codebrew@gmail.com>
2015-07-11 18:27:31 +0530

    Final

--- app/src/main/java/com/example/code_brew/myapplication/MainActivity.java
+++ app/src/main/java/com/example/code_brew/myapplication/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         click.setOnClickListener(this);
         // Just Testing
 
+        // Yes Here
     }
 
     @Override
